2a0dd850920bf165dd12c1f539c41a49b7591141
nisha <nisha.4r@gmail.com>
2023-04-24 23:10:56 -0600

    checkin table of contents

--- createmarkdown.js
+++ createmarkdown.js
@@ -1,4 +1,23 @@
 function createMarkDown(data){
-return  "hello" ;
+
+    let response = 
+    `
+
+    ## Title
+    ${data.title}
+
+    ## Description
+    ${data.description};
+
+    ## Table of Contents
+    1. [Installation] 
+    2. [Usage]
+    3. [License]
+    4. [Contributing]
+    5. [Tests]
+    6. [License]
+    6. [Developer]
+
+    `;
 }
 module.exports = createMarkDown;
